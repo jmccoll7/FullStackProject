2dea982b770a53cd977ee644c54515e1b356cae9
James McColl <jmccoll71@gmail.com>
2022-08-27 18:35:10 -0500

    38:00

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+import { MikroORM, RequiredEntityData } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.fork({}).create(Post, {
+    title: "my first post",
+  } as RequiredEntityData<Post> );
+  await orm.em.persistAndFlush(post);
+
+};
+
+main().catch(err => {
+  console.error(err)
+});
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core"
+import path from 'path';
+
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "/migrations"),
+    glob: '!(*.d).{js,ts}'
+  },
+  allowGlobalContext: true,
+  entities: [Post],
+  dbName: 'postgres',
+  user: 'postgres',
+  password: 'Don\'t @ me ok?',
+  type: 'postgresql',
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
