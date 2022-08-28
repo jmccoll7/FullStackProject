22b9cf4fcf2f2265a51dab4098def487966929f7
James McColl <jmccoll71@gmail.com>
2022-08-27 20:04:20 -0500

    56:29

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,23 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
 
+  @Field(() => Int)
   @PrimaryKey()
   _id!: number;
 
+  @Field(() => Date)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => Date)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { MikroORM, RequiredEntityData } from "@mikro-orm/core"
+import "reflect-metadata";
+import { MikroORM, RequestContext } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express'
 import {buildSchema} from 'type-graphql'
 import { HelloResolver } from "./resolvers/hello";
-import { Server } from "http";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
 
@@ -20,12 +20,17 @@ const main = async () => {
 
   const app = express();
 
+  app.use((req, res, next) => {
+    RequestContext.create(orm.em, next)
+  })
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false
-    })
-  })
+    }),
+    context: () => ({em: orm.em})
+  });
 
   await apolloServer.start()
   apolloServer.applyMiddleware({app})
