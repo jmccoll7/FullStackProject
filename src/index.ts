35e5d17932dcd99b368d851bc1571fc1f8884fac
James McColl <jmccoll71@gmail.com>
2022-08-27 19:10:31 -0500

    46:00

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
+import { Server } from "http";
 
 const main = async () => {
 
@@ -15,9 +19,17 @@ const main = async () => {
   // console.log(posts)
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send("hello");
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
   })
+
+  await apolloServer.start()
+  apolloServer.applyMiddleware({app})
+
   app.listen(4000, () => {
     console.log('server started on localhost:4000')
   })
